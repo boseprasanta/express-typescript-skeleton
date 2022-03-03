a4b2cf2a35977bf3d519fa63bb8e90e0b788a24b
boseprasanta <pbose1310@gmail.com>
2022-03-04 05:14:27 +0530

    chore(package.json, package-lock.json): ts definition for Node added and repo is made cz friendly
    Typescript definition added for Node.js to use cluser to run multiple instances at once.
    cz-conventional-changelog package added to make repo commitizen friendly

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import cluster from 'cluster'
+import { cpus } from 'os'
 
 import App from "./App"
 import config from "./config/config"
@@ -6,6 +8,11 @@ import config from "./config/config"
 const app = new App().express
 const port = config.port
 
-app.listen(port, () => {
-    console.log(`API is listening on http://localhost:${port}`);
-});
+if (cluster.isPrimary === true) {
+    const CPUS: any = cpus()
+    CPUS.forEach(() => cluster.fork())
+} else {
+    app.listen(port, () => {
+        console.log(`API is listening on http://localhost:${port} on ${process.pid}`);
+    });
+}
